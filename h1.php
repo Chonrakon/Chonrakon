8468f6b915dc69fd727247b958dc36a769f170b6
Chonrakon <24491@srithatpit.ac.th>
2025-03-04 11:39:06 +0700

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>สร้าง table</title>
+</head>
+<body>
+    <div>
+        <p>
+            <h1>การสร้างตาราง</h1>
+        </p>
+        <table boder="1" aling="center">
+            <thead>
+                <td>รหัสสินค้า</td>
+                <td>ซื้อสินค้า</td>
+                <td>ภาพประกอบ</td>
+                <td>ราคา</td>
+            </thead>
+
+        </table>
+    </div>
+</body>
+</html>
